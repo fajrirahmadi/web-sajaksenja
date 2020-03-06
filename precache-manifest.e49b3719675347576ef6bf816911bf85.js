878a9ef0084bde5d7c2c82c861776eb3b96f3740
Fajri Rahmadi <fajri.rahmadi@strongbee.co.id>
2020-03-06 23:00:49 +0700

    Updates

---
+++ precache-manifest.e49b3719675347576ef6bf816911bf85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6369c8ce42f3a55d8966540605dcf43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df17939c7891165b04c",
+    "url": "/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "613e546d557ca1a9fb99",
+    "url": "/static/css/main.30ca1f65.chunk.css"
+  },
+  {
+    "revision": "3df17939c7891165b04c",
+    "url": "/static/js/2.d9e16a03.chunk.js"
+  },
+  {
+    "revision": "613e546d557ca1a9fb99",
+    "url": "/static/js/main.7e1547e5.chunk.js"
+  },
+  {
+    "revision": "47854bde7706e1720146",
+    "url": "/static/js/runtime-main.b34489f9.js"
+  },
+  {
+    "revision": "bf1589cdb51f164bc47c87548e6eb579",
+    "url": "/static/media/sajak-senja-icon.bf1589cd.png"
+  }
+]);
